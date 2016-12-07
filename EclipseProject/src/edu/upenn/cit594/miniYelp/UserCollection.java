18477969dd760f410f34847d298321394631e24d
ziyiyan <ziyiyan94@gmail.com>
2016-12-06 20:47:34 -0500

    user and restaurant class added

---
+++ EclipseProject/src/edu/upenn/cit594/miniYelp/UserCollection.java
@@ -0,0 +1,12 @@
+package edu.upenn.cit594.miniYelp;
+
+import java.util.HashMap;
+
+public class UserCollection {
+	private HashMap<Integer, User> userCollection;
+	
+	public UserCollection(){
+		this.userCollection = new HashMap<Integer,User>();
+	}
+
+}
---
+++ EclipseProject/src/test/tesetImport.java
@@ -0,0 +1,9 @@
+package test;
+
+public class tesetImport {
+
+	public static void main(String[] args) {
+		System.out.println("aaa");
+	}
+
+}
