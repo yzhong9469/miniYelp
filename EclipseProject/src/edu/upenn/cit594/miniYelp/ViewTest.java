6d4ac7105ece34e12689c8797b17690617f3a5c8
yzhong9469 <yzhong@oberlin.edu>
2016-12-09 22:19:12 -0500

    Upload two background images

--- EclipseProject/src/edu/upenn/cit594/miniYelp/ViewTest.java
+++ EclipseProject/src/edu/upenn/cit594/miniYelp/ViewTest.java
@@ -1,9 +1,46 @@
 package edu.upenn.cit594.miniYelp;
 
+import java.awt.GridBagLayout;
+import java.awt.*;
+
+import javax.swing.ImageIcon;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+
 public class ViewTest {
 
 	public static void main(String[] args) {
-		new SearchView().setVisible(true);
+		JFrame miniYelp = new JFrame();
+		miniYelp.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		miniYelp.setSize(800, 500);
+		miniYelp.setVisible(true);
+		
+//		
+//		JLabel background=new JLabel(new ImageIcon("U-Penn.jpg"));
+//        miniYelp.add(background);
+//        miniYelp.setLayout(new FlowLayout());
+//        miniYelp.setVisible(true);
+		
+		
+        
+		JPanel login = new LoginView();
+		login.setVisible(true);
+		miniYelp.add(login);
+		//miniYelp.pack();
+		
+		miniYelp.setVisible(true);
+//		miniYelp.setVisible(true);
+//		
+//		JPanel search = new SearchView();
+//		search.setVisible(true);
+//		miniYelp.add(search);
+//		miniYelp.setVisible(true);
+//		miniYelp.remove(search);
+		
+		
+		//new SearchView().setVisible(true);
 
 	}
 
