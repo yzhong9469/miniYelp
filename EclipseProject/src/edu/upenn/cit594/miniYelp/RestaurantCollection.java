eee99571a2d8713c35de51bc7b7e419e7cc014ef
ziyiyan <ziyiyan94@gmail.com>
2016-12-11 17:22:17 -0500

    Add comments in restaurantcollection class

--- EclipseProject/src/edu/upenn/cit594/miniYelp/RestaurantCollection.java
+++ EclipseProject/src/edu/upenn/cit594/miniYelp/RestaurantCollection.java
@@ -90,18 +90,22 @@ public class RestaurantCollection {
 	
 
 	/**
-	 * 
+	 * Get category count 
+	 * @return a HashMap that key is category and value is category count
 	 */
 	public HashMap<String,Integer> getCategorycount(){
 		categorycount = new HashMap<String,Integer>();
+		//update category count 
 		for (String id:this.restaurants.keySet()){
 			List<String> categories = this.restaurants.get(id).getCategories();
 			for (int i = 0; i < categories.size(); i++){
 				String cg = categories.get(i);
+				//if the category is already in the HashMap, update counting
 				if(categorycount.containsKey(cg)){
 					int count = categorycount.get(cg);
 					count++;
 					categorycount.replace(cg, count);
+					//if the category is not in the HashMap, add the category in it
 				} else {
 					categorycount.put(cg, 1);
 				}
@@ -109,22 +113,30 @@ public class RestaurantCollection {
 		}
 		return categorycount;
 	}
-
+	
+	/**
+	 * Get a list of categories
+	 * @return a list of categories
+	 */
 	public List<String> getCategoryilst(){
 		List<Integer> count = new ArrayList<Integer>();
 		HashMap<String,Integer> frequency = this.getCategorycount();
+		//add category counts in the list
 		for (String key:frequency.keySet()){
 			count.add(frequency.get(key));
 		}
+		//sort the list and keep the highest 20 counts
 		Collections.sort(count,Collections.reverseOrder());
 		count = count.subList(0, 19);
 		categorylist = new HashSet<String>();
+		//get the list of categories with highest 20 counts
 		for (String key:frequency.keySet())
 			for (int i = 0; i<count.size(); i++){
 				if (frequency.get(key) == count.get(i)){
 					categorylist.add(key);
 				}
 			}
+		//sort the category in alphabetical order
 		List<String> sortlist = new ArrayList<String>(categorylist);
 		Collections.sort(sortlist);
 	    return sortlist;
