feeedf866d2eb1929ec1742582bcdf36ca4a7537
Man Hu <human0110@gmail.com>
2016-12-10 22:14:26 -0500

    revise recommendRestaurant() method

--- EclipseProject/src/edu/upenn/cit594/miniYelp/RecommendRestaurant.java
+++ EclipseProject/src/edu/upenn/cit594/miniYelp/RecommendRestaurant.java
@@ -3,109 +3,124 @@ package edu.upenn.cit594.miniYelp;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.PriorityQueue;
+import java.util.Random;
 
 public class RecommendRestaurant implements Recommend {
-	private RestaurantFileParser restaurantParser;
-
-	public RecommendRestaurant(){
-		restaurantParser = new RestaurantFileParser();
+	RestaurantCollection rc;
+	
+	public RecommendRestaurant() {
+		rc = RestaurantCollection.getInstance();
 	}
 
-
-	public ArrayList<Restaurant> getHighRatings(User user){
-		ArrayList<Restaurant> list = new ArrayList<Restaurant>();
-		HashMap<Restaurant,Integer> userRatings = user.getRatings();
-		for (Restaurant restaurant:userRatings.keySet()){
-			if (userRatings.get(restaurant) >= restaurant.getRating()){
-				list.add(restaurant);
+	private ArrayList<String> getHighRatedRestaurantID(User user){
+		ArrayList<String> list = new ArrayList<>();
+		HashMap<String, Double> userRatings = user.getRatings();
+		
+		for (String id : userRatings.keySet()) {
+			if (userRatings.get(id) >= rc.getRestaurant(id).getRating()){
+				list.add(id);
 			}
 		}
 		return list;
 	}
+	
+	private class Point implements Comparable<Point> {
+		String id;
+		int count;
+		
+		public Point(String id, int count) {
+			this.id = id;
+			this.count = count;
+		}
 
-	public HashMap<String,Integer> getCategory(ArrayList<Restaurant> resList){
+		@Override
+		public int compareTo(Point o) {
+			return this.count - o.count;
+		}
+		
+		public String getId() {
+			return id;
+		}
+		
+	}
+	
+
+	public HashSet<String> getUserFavoriteCategories(ArrayList<String> resList){
 		HashMap<String,Integer> categoryCt = new HashMap<String,Integer>();
+		
+		//count the number of the appearance of each category
 		for (int i = 0; i < resList.size(); i++){
-			List<String> categories = resList.get(i).getCategories();
+			List<String> categories = rc.getRestaurant(resList.get(i)).getCategories();
 			for (int j = 0; j < categories.size(); j++){
-				int ct;
-				if (categoryCt.containsKey(categories.get(j))){
-					ct = categoryCt.get(categories.get(j));
-					categoryCt.replace(categories.get(j), ct+1);
-				} else {
-					categoryCt.put(categories.get(j),1);
-				}
+				Integer ct = categoryCt.get(categories.get(j));
+				if (ct == null) ct = 0;
+				categoryCt.put(categories.get(i), ++ct);
 			}
 		}
-		return categoryCt;
+		
+		//select the first five highest categories
+		PriorityQueue<Point> pq = new PriorityQueue<>();
+		for (String category : categoryCt.keySet()) {
+			Point p = new Point(category, categoryCt.get(category));
+			pq.add(p);
+			if (pq.size() > 5) pq.poll();
+		}
+		
+		HashSet<String> favoriteCategories = new HashSet<String>();
+		while (!pq.isEmpty()) {
+			favoriteCategories.add(pq.poll().getId());
+		}
+		
+		return favoriteCategories;
 	}
+	
 
-	public ArrayList<String> selectCategory(HashMap<String,Integer> categoryCt){
-		ArrayList<String> cg = new ArrayList<String>();
-		if (categoryCt.size() <= 5){
-			for (String category:categoryCt.keySet()){
-				cg.add(category);
-			}
-		} else {
-			ArrayList<Integer> count = new ArrayList<Integer>();
-			for (String cat:categoryCt.keySet()){
-				count.add(categoryCt.get(cat));
-			}
-			Collections.sort(count);
-			Collections.reverse(count);
-			for (int k = 0; k < 5; k++){
-				for (String cat:categoryCt.keySet()){
-					if (categoryCt.get(cat) == count.get(k)){
-						cg.add(cat);
-					}
-				}
-			}
-		}
-		return cg;
-	}
-
-	public List<Restaurant> getReclist(User user){
-		List<Restaurant> reclist = new ArrayList<Restaurant>();
-		List<Restaurant> reslist = restaurantParser.getRestaurantlist();
-		if (user.getRatings().isEmpty()){
-			for (int i = 0; i < reslist.size(); i++){
-				if(reslist.get(i).getRating() >= 4.5){
-					reclist.add(reslist.get(i));
-				}
-			}
-			Collections.shuffle(reclist);
-		} else {
-			ArrayList<Restaurant> highRatings = getHighRatings(user);
-			HashMap<String,Integer> categoryCt = getCategory(highRatings);
-			ArrayList<String> selectCat = selectCategory(categoryCt);
-			for (int i = 0; i < selectCat.size(); i++){
-				String cg = selectCat.get(i);
-				for (int j = 0; j < reslist.size(); i++){
-					if (reslist.get(j).getCategories().contains(cg) && reslist.get(j).getRating() >= 4.5
-							&& !user.getRatings().containsKey(reslist.get(j))){
-						reclist.add(reslist.get(j));
-					}
-				}
-				if (reclist.size() < 10){
-					for (int k = 0; k < reslist.size(); i++){
-						while(reclist.size()<10){
-							if (reslist.get(k).getCategories().contains(cg) && reslist.get(k).getRating() == 4
-									&& !user.getRatings().containsKey(reslist.get(k))){
-								reclist.add(reslist.get(k));
-							}
-						}
-					}
-				}
-			}
-			Collections.shuffle(reclist);
-		}
-		return reclist.subList(0,10);
-	}
 	@Override
-	public List<Restaurant> recommRestaurant() {
-		// TODO Auto-generated method stub
-		return null;
+	public List<Restaurant> recommRestaurant(User user) {
+		ArrayList<Restaurant> result = new ArrayList<>();
+		Iterator<String> it = rc.getAllRestaurants().iterator();
+		
+		if (user.getRatings().isEmpty()){
+			while (it.hasNext()) {
+				Restaurant r = rc.getRestaurant(it.next());
+				if (r.getRating() >= 4.5) result.add(r);
+			} 
+			
+			Collections.shuffle(result);
+			return result.subList(0, Math.min(10, result.size()));
+		}
+		
+		ArrayList<String> restIDs = getHighRatedRestaurantID(user);
+		HashSet<String> favoriteCategories = getUserFavoriteCategories(restIDs);
+		ArrayList<Restaurant> backup = new ArrayList<>();
+		
+		while (it.hasNext()) {
+			Restaurant r = rc.getRestaurant(it.next());
+			if (user.ratedRestaurantBefore(r.getId()) || !r.containCategories(favoriteCategories)) continue;
+			
+			if (r.getRating() >= 4.5) result.add(r);
+			else if (r.getRating() == 4) backup.add(r);
+			else continue;
+		}
+		
+		int index = new Random().nextInt(backup.size());
+		int s1 = result.size(), s2 = backup.size();
+		
+		if (s1 + s2 < 10) {
+			result.addAll(backup);
+		} else if (s1 < 10){
+			for (int i = 0; i < 10 - s1; i++) {
+				result.add(backup.get((i + index) % s2));
+			}
+		}
+		
+		Collections.shuffle(result);
+		return result.subList(0, Math.min(10, result.size()));
 	}
-
+	
 }
